ab603850cc4ddbddbe986d6799ad35dac0586352
kindlingscript <inflamedmuse@gmail.com>
2016-11-30 16:00:07 -0600

    Button CSS, addition of Browsersync ❤️

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,37 @@ var gulp = require('gulp'),
 watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
-css_variables = require('postcss-simple-vars'),
+cssVariables = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-css_import = require('postcss-import');
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
 gulp.task('default', function() {
   console.log('This is the default Gulp task!');
 });
 
-gulp.task('html', function() {
-  console.log('Imagine something useful being done to your HTML here.');
-});
-
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/style.css')
-    .pipe(postcss([css_import, css_variables, nested, autoprefixer]))
+    .pipe(postcss([cssImport, cssVariables, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function() {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
   watch('./app/index.html', function() {
-    gulp.start('html');
+    browserSync.reload();
   });
   watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('styles');
-  })
+    gulp.start('cssInject');
+  });
+});
+
+gulp.task('cssInject', ['styles'] , function() {
+  return gulp.src('./app/temp/styles/style.css')
+    .pipe(browserSync.stream());
 });
