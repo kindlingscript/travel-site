8dabb384a941b2427b3685f2960e5022c33798fc
kindlingscript <inflamedmuse@gmail.com>
2017-01-23 23:45:41 -0600

    Finished up custom modal functionality

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,8 +2,10 @@ import MobileMenu from './modules/mobile-menu';
 import RevealOnScroll from './modules/reveal-on-scroll';
 import $ from 'jquery';
 import StickyHeader from './modules/sticky-header';
+import Modal from './modules/modal';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), "85%");
 new RevealOnScroll($('.testimonial'), "60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
